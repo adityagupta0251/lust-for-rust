3326754801d26b321c5cfe1a37e4051640635bc4
adityagupta0251 <itsokkadi0251@gmail.com>
2026-04-16 23:27:17 +0530

    completed methods

---
+++ 07/structs_method/src/main.rs
@@ -0,0 +1,54 @@
+/*
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+        
+}
+
+impl Rectangle {
+    // In Rust, the impl (implementation) keyword is used to define the functionality of a type. It serves two primary purposes: defining methods for a specific type and implementing shared behaviors (traits) for a type.
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+
+fn main() {
+    let rect1 =  Rectangle {
+        width: 30,
+        height: 50,
+        
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
+*/
+
+// Self Keyword
+/*
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+        
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+        
+    };
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+}
+*/
+
+
+
